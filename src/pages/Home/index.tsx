51ccaf97bfe868579a53f484c4debacdfe044cb1
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2024-06-05 22:27:09 -0300

    feat: created initial components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
+import styles from "./home.module.css";
+
 export function Home() {
   return (
     <div>
-      <h1>Home2</h1>
+      <h1 className={styles.title}>Home2</h1>
     </div>
   );
 }
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
-import { Home } from '../pages/Home';
+import { Home } from "../pages/Home";
+import { Login } from "../pages/Login";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
